09877bff24f3e5bf62bd009ca1d064ab461724c0
kuritan <lizi103@hotmail.com>
2021-03-08 16:29:09 +0900

    get todo

--- main.go
+++ main.go
@@ -17,15 +17,15 @@ func main() {
 	*/
 
 	// fmt.Println(models.Db)
+	/*
+		u := &models.User{}
+		u.Name = "test"
+		u.Email = "test@example.com"
+		u.PassWord = "testtest"
+		fmt.Println(u)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
-
-	u.CreateUser()
-
+		u.CreateUser()
+	*/
 	/*
 		u, _ := models.GetUser(1)
 		fmt.Println(u)
@@ -40,7 +40,11 @@ func main() {
 		u, _ = models.GetUser(1)
 		fmt.Println(u)
 	*/
+	/*
+		user, _ := models.GetUser(2)
+		user.CreateTodo("First Todo")
+	*/
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("First Todo")
+	t, _ := models.GetTodo(1)
+	fmt.Println(t)
 }
