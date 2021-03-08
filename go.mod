2911ccfdb6a701a2e4a34f351fa3bb0da1e7e257
kuritan <lizi103@hotmail.com>
2021-03-08 13:33:56 +0900

    test print

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-to-do
+
+go 1.16
+
+require gopkg.in/ini.v1 v1.62.0 // indirect
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"go-to-do/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+
+	log.Println("test")
+}
