e946dc602ecbdd39fa5151a4511d59aafcdc048a
kuritan <lizi103@hotmail.com>
2021-03-08 14:28:03 +0900

    add a test user

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-to-do
 go 1.16
 
 require (
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.6 // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
 )
--- main.go
+++ main.go
@@ -15,5 +15,13 @@ func main() {
 		log.Println("test")
 	*/
 
-	fmt.Println(models.Db)
+	// fmt.Println(models.Db)
+
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
 }
