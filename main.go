20061d98db6af3969d617397a5980572088f08f9
kuritan <lizi103@hotmail.com>
2021-03-08 15:58:42 +0900

    update test user

--- main.go
+++ main.go
@@ -17,11 +17,22 @@ func main() {
 
 	// fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
+	/*
+		u := &models.User{}
+		u.Name = "test"
+		u.Email = "test@example.com"
+		u.PassWord = "testtest"
+		fmt.Println(u)
+
+		u.CreateUser()
+	*/
+
+	u, _ := models.GetUser(1)
 	fmt.Println(u)
 
-	u.CreateUser()
+	u.Name = "Test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
 }
